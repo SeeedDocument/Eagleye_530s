2e9391ae0fb1f17f77cb125244997012dbc24fce
xyu6 <yu.bill@hotmail.com>
2018-04-04 14:44:00 +0800

    update

---
+++ res/recorder_2Mics.py
@@ -0,0 +1,34 @@
+import pyaudio
+import wave
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 10
+WAVE_OUTPUT_FILENAME = "test.wav"
+
+audio = pyaudio.PyAudio()
+
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+print "recording audio..."
+frames = []
+
+threshold = 800
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+   data = stream.read(CHUNK)
+   frames.append(data)
+print "done recording"
+
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
